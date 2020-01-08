2e01985810673686a208bc91170dd477c8314dd3
Michael Shkolnik <michaels@surfsight.net>
2020-01-08 13:58:55 +0200

    Adding more tests

--- src/index.test.js
+++ src/index.test.js
@@ -5,4 +5,25 @@ describe('starwars-names', function() {
     it('should work!', function() {
         expect(true).to.be.true;
     });
+
+    describe('all', function() {
+        it('should be an array of strings', function() {
+           expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+           function isArrayOfStrings(array){
+               return array.every(item => typeof item ==='string');
+           }
+        });
+
+        it('should contain `Luke Skywalker`', function() {
+           expect(starWars.all).to.include('Luke Skywalker');
+        });
+    });
+
+    describe('random', function() {
+       it('should return a random item from the starWars.all', function() {
+          const randomItem = starWars.random();
+          expect(starWars.all).to.include(randomItem);
+       });
+    });
 });
